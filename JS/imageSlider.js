9944556781e2c95cb68016691a368b97d1acbe80
NovaDA <ayoublaissaoui.1@gmail.com>
2022-08-18 21:28:53 +0100

    added Habbit Buddy

--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -1,4 +1,6 @@
+const title = document.getElementById('page-title').textContent
 const images = ["../images/gh_01.jpg", "../images/gh_02.jpg", "../images/gh_03.jpg" , "../images/gh_04.jpg", "../images/gh_05.jpg"]
+const imagesHabbitBuddy = ["../images/Raad1.png", "../images/Raad2.png", "../images/Raad3.png", "../images/Raad1.png", "../images/Raad2.png"]
 // const carousel = document.querySelector(".card-img-top");
 const image = document.getElementById("image-slider")
 const interval = setInterval(function() {
@@ -8,7 +10,12 @@ const interval = setInterval(function() {
 var index = 1;
 
 startCarousel = () =>{
-    image.src = images[index++];
+    if(title === 'GHOST HUNTERS'){
+        image.src = images[index++];
+    }else{
+        image.src = imagesHabbitBuddy[index++];
+    }
+    
     image.classList.remove("fade");
     void image.offsetWidth;
     image.classList.add("fade");
