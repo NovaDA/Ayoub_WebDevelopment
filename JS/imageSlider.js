91aba2d11caabca286b89afacdedc9d0a0565cfd
NovaDA <ayoublaissaoui.1@gmail.com>
2022-08-19 00:51:22 +0100

    added Question Roulette , Brighter Minds Pages

--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -1,6 +1,10 @@
 const title = document.getElementById('page-title').textContent
 const images = ["../images/gh_01.jpg", "../images/gh_02.jpg", "../images/gh_03.jpg" , "../images/gh_04.jpg", "../images/gh_05.jpg"]
-const imagesHabbitBuddy = ["../images/Raad1.png", "../images/Raad2.png", "../images/Raad3.png", "../images/Raad1.png", "../images/Raad2.png"]
+const imagesBH = ["../images/Raad1.png", "../images/Raad2.png", "../images/Raad3.png", "../images/Raad1.png", "../images/Raad2.png"]
+const imagesQR = ["../images/QuestionRoulette01.png", "../images/QuestionRoulette02.png", "../images/QuestionRoulette03.png", "../images/QuestionRoulette04.png", 
+"../images/QuestionRoulette05.png", "../images/QuestionRoulette06.png"]
+const imagesHM = ["../images/HappyMinds1.png", "../images/HappyMinds2.png", "../images/HappyMinds3.png", "../images/HappyMinds4.png", "../images/HappyMinds5.png", "../images/HappyMinds6.png"
+, "../images/HappyMinds7.png", "../images/HappyMinds8.png", "../images/HappyMinds9.png", "../images/HappyMinds21.png", "../images/HappyMinds22.png", "../images/HappyMinds23.png", "../images/HappyMinds24.png", , "../images/HappyMinds25.png"]
 // const carousel = document.querySelector(".card-img-top");
 const image = document.getElementById("image-slider")
 const interval = setInterval(function() {
@@ -11,9 +15,17 @@ var index = 1;
 
 startCarousel = () =>{
     if(title === 'GHOST HUNTERS'){
+        if(index > images.length - 1) index = 0;
         image.src = images[index++];
+    }else if(title === 'HABBIT BUDDY'){
+        if(index > imagesBH.length - 1) index = 0;
+        image.src = imagesBH[index++];
+    }else if(title === 'QUESTION ROULETTE'){
+        if(index > imagesQR.length - 1) index = 0;
+        image.src = imagesQR[index++];
     }else{
-        image.src = imagesHabbitBuddy[index++];
+        if(index > imagesHM.length - 1) index = 0;
+        image.src = imagesHM[index++];
     }
     
     image.classList.remove("fade");
