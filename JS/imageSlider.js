81ad7949f8c4fe41125bb610e56c52955fc7ed85
NovaDA <ayoublaissaoui.1@gmail.com>
2022-08-20 12:43:53 +0100

    Updated all webpages with same features

--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -5,6 +5,7 @@ const imagesQR = ["../images/QuestionRoulette01.png", "../images/QuestionRoulett
 "../images/QuestionRoulette05.png", "../images/QuestionRoulette06.png"]
 const imagesHM = ["../images/HappyMinds1.png", "../images/HappyMinds2.png", "../images/HappyMinds3.png", "../images/HappyMinds4.png", "../images/HappyMinds5.png", "../images/HappyMinds6.png"
 , "../images/HappyMinds7.png", "../images/HappyMinds8.png", "../images/HappyMinds9.png", "../images/HappyMinds21.png", "../images/HappyMinds22.png", "../images/HappyMinds23.png", "../images/HappyMinds24.png", , "../images/HappyMinds25.png"]
+const imagesPP = ["../images/gh_01.jpg", "../images/Raad1.png", "../images/QuestionRoulette01.png", "../images/HappyMinds1.png"]
 // const carousel = document.querySelector(".card-img-top");
 const image = document.getElementById("image-slider")
 const interval = setInterval(function() {
@@ -23,6 +24,9 @@ startCarousel = () =>{
     }else if(title === 'QUESTION ROULETTE'){
         if(index > imagesQR.length - 1) index = 0;
         image.src = imagesQR[index++];
+    }else if(title === 'AYOUB LAISSAOUI'){
+        if(index > imagesPP.length - 1) index = 0;
+        image.src = imagesPP[index++];
     }else{
         if(index > imagesHM.length - 1) index = 0;
         image.src = imagesHM[index++];
